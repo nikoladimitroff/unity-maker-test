0a2f7dfd1e488887cbff2b4a9491ad5540beb743
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2021-12-08 02:44:10 +0200

    Initial commit and setup of the basic UI screen interactions.

---
+++ Assets/AppModeManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AppModeManager : MonoBehaviour
+{
+    // This might need to be converted to a Dictionary but given we only have 3 screens, a list is just as good
+    public List<GameObject> ScreenList;
+    private ScreenType ActiveScreen;
+
+    private void OnValidate()
+    {
+        Debug.Assert(ScreenList.Count > 0, "The ModeManager requires at least 1 screen!");
+        foreach (GameObject screenRoot in ScreenList)
+        {
+            Debug.Assert(screenRoot != null, "A screen must be non-null");
+            UIViewModel viewModel = screenRoot.GetComponent<UIViewModel>();
+            Debug.Assert(viewModel != null, "A screen must have a UIViewModel component!");
+        }
+    }
+    void Start()
+    {
+        ActiveScreen = ScreenList[0].GetComponent<UIViewModel>().ViewModelScreenType;
+        ActivateScreen(ActiveScreen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateScreen(string nextScreenName)
+    {
+        ScreenType nextScreen;
+        if (!System.Enum.TryParse<ScreenType>(nextScreenName, out nextScreen))
+        {
+            Debug.LogErrorFormat("Invalid name for a screen: {}", nextScreenName);
+            return;
+        }
+
+        ActivateScreen(nextScreen);
+    }
+
+    private void ActivateScreen(ScreenType nextScreen)
+    {
+        foreach (GameObject screenRoot in ScreenList)
+        {
+            UIViewModel viewModel = screenRoot.GetComponent<UIViewModel>();
+            screenRoot.SetActive(viewModel.ViewModelScreenType == nextScreen);
+        }
+        ActiveScreen = nextScreen;
+        Debug.LogFormat("Activating screen {0}", nextScreen);
+    }
+}
---
+++ Assets/UI/UIViewModel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ScreenType
+{
+    Create,
+    Place,
+    Test
+}
+
+public class UIViewModel : MonoBehaviour
+{
+    public ScreenType ViewModelScreenType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
